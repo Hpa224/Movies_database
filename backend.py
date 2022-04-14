7d7c87182937128783e5e1e1a46eef557363bc9e
Patrick Heron <hpa434@gmail.com>
2022-04-14 01:17:40 -0400

    fixed incorrect commit

--- backend.py
+++ backend.py
@@ -2,65 +2,64 @@ from ast import Return
 import sqlite3
 
 def connect():
-    conn = sqlite3.connect("servers.db")
+    conn = sqlite3.connect("movies.db")
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS server (id INTEGER PRIMARY KEY, name text)")
-    cur.execute("CREATE TABLE IF NOT EXISTS maps (id INTEGER, name text)")
+    cur.execute("CREATE TABLE IF NOT EXISTS movie (id INTEGER PRIMARY KEY, title text, director text, year integer, rating text)")
     conn.commit()
     conn.close()
 
 
-def insert(name):
-    conn = sqlite3.connect("servers.db")
+def insert(title, director, year, rating):
+    conn = sqlite3.connect("movies.db")
     cur = conn.cursor()
-    cur.execute("INSERT INTO server VALUES (NULL,?)", (name,))
+    cur.execute("INSERT INTO movie VALUES (NULL,?,?,?,?)", (title, director, year, rating))
     conn.commit()
     conn.close()
 
 
 def view():
-    conn = sqlite3.connect("servers.db")
+    conn = sqlite3.connect("movies.db")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM server")
+    cur.execute("SELECT * FROM movie")
     rows = cur.fetchall()
     conn.close()
     return rows
 
 def view_2():
-    conn = sqlite3.connect("servers.db")
-    table = conn.execute("SELECT * FROM server")
+    conn = sqlite3.connect("movies.db")
+    table = conn.execute("SELECT * FROM movie")
     for i in table:
         print(i)
     conn.close()
 
 def search(title="", director="", year="", rating=""):
-    conn = sqlite3.connect("servers.db")
+    conn = sqlite3.connect("movies.db")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM server WHERE title=? OR director=? or year=? or rating=?", (title, director, year, rating))
+    cur.execute("SELECT * FROM movie WHERE title=? OR director=? or year=? or rating=?", (title, director, year, rating))
     rows = cur.fetchall()
     conn.close()
     return rows
 
 
 def delete(id):
-    conn = sqlite3.connect("servers.db")
+    conn = sqlite3.connect("movies.db")
     cur = conn.cursor()
-    cur.execute("DELETE FROM server WHERE id=?", (id,))
+    cur.execute("DELETE FROM movie WHERE id=?", (id,))
     conn.commit()
     conn.close()
 
 
 def update(id, title, director, year, rating):
-    conn = sqlite3.connect("servers.db")
+    conn = sqlite3.connect("movies.db")
     cur = conn.cursor()
-    cur.execute("UPDATE server SET title=?, director=?, year=?, rating=? WHERE id=?", (title, director, year, rating, id))
+    cur.execute("UPDATE movie SET title=?, director=?, year=?, rating=? WHERE id=?", (title, director, year, rating, id))
     conn.commit()
     conn.close()
 
 def selection(id):
-    conn = sqlite3.connect("servers.db")
+    conn = sqlite3.connect("movies.db")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM server WHERE id=?", (id,))
+    cur.execute("SELECT * FROM movie WHERE id=?", (id,))
     rows = cur.fetchall()
     conn.close()
     return rows
@@ -68,7 +67,7 @@ def selection(id):
 connect()
 # update()
 # delete(7)
-# insert("")
+insert("Sicario", "Forgot", 2017, "R")
 # view_2()
 # print()
 print(view())
--- gui.py
+++ gui.py
@@ -1,54 +1,118 @@
 from tkinter import *
 import backend
-import app
 
 
-def server_command():
-    entry = app.server_command(server_text.get())
-    list1.insert(END, entry[0], entry[1], f"{entry[2]}/{entry[3]}", "")
+def view_command():
+    list1.delete(0, END)
+    for i in backend.view():
+        entry = f"{i[0]} | {i[1]} | {i[2]} | {i[3]} | {i[4]}"
+        list1.insert(END, entry)
+
+
+def search_command():
+    list1.delete(0, END)
+    for i in backend.search(title_text.get(), director_text.get(), year_text.get(), rating_text.get()):
+        entry = f"{i[0]} | {i[1]} | {i[2]} | {i[3]} | {i[4]}"
+        list1.insert(END, entry)
+
+
+def add_command():
+    backend.insert(title_text.get(), director_text.get(), year_text.get(), rating_text.get())
+    entry = f"{backend.view()[-1][0]} | {title_text.get()} | {director_text.get()} | {year_text.get()} | {rating_text.get()}"
+    list1.insert(END, entry)
     e1.delete(0, END)
-    backend.insert(entry[0])
+    e2.delete(0, END)
+    e3.delete(0, END)
+    e4.delete(0, END)
 
+
+def get_selected_row(event):
+    global index
+    index = list1.curselection()[0]+1
+    selection = list1.get(list1.curselection())
+    print(selection.split(" | "))
+    try:
+        e1.delete(0, END)
+        e1.insert(END, selection.split(" | ")[1])
+        e2.delete(0, END)
+        e2.insert(END, selection.split(" | ")[2])
+        e3.delete(0, END)
+        e3.insert(END, selection.split(" | ")[3])
+        e4.delete(0, END)
+        e4.insert(END, selection.split(" | ")[4])
+    except IndexError:
+        pass
+
+
+def delete_command():
+    backend.delete(index)
+    list1.delete(list1.curselection())
+    e1.delete(0, END)
+    e2.delete(0, END)
+    e3.delete(0, END)
+    e4.delete(0, END)
+
+def update_command():
+    backend.update(index, title_text.get(), director_text.get(), year_text.get(), rating_text.get())
+    view_command()
 
 window = Tk()
-window.wm_title("CS:GO Notifier")
+window.wm_title("Movies")
 
-# small_font = tkFont.Font(size=5)
-
-l1 = Label(window, text="Server IP")
+l1 = Label(window, text="Title")
 l1.grid(row=0, column=0)
 
-l2 = Label(window, text="Favorites")
-l2.grid(row=0, column=3)
+l2 = Label(window, text="Director")
+l2.grid(row=0, column=2)
 
-server_text = StringVar()
-e1 = Entry(window, textvariable=server_text, width=55)
-e1.insert(0, "216.52.148.47:27015")
+l3 = Label(window, text="Year")
+l3.grid(row=1, column=0)
+
+l4 = Label(window, text="Rating")
+l4.grid(row=1, column=2)
+
+title_text = StringVar()
+e1 = Entry(window, textvariable=title_text)
 e1.grid(row=0, column=1)
 
-favorites_text = StringVar()
-e2 = Entry(window, textvariable=favorites_text, width=55)
-e2.grid(row=0, column=4)
+director_text = StringVar()
+e2 = Entry(window, textvariable=director_text)
+e2.grid(row=0, column=3)
 
-b1 = Button(window, text="Add", width=3, command=server_command)
-b1.grid(row=0, column=2)
+year_text = StringVar()
+e3 = Entry(window, textvariable=year_text)
+e3.grid(row=1, column=1)
 
-b2 = Button(window, text="Add", width=3)
-b2.grid(row=0, column=5)
+rating_text = StringVar()
+e4 = Entry(window, textvariable=rating_text)
+e4.grid(row=1, column=3)
 
-list1 = Listbox(window, height=8, width=55, font=small_font)
-list1.grid(row=2, column=1)
-# list1.grid(row=2, column=1, columnspan=2)
+list1 = Listbox(window, height=6, width=35)
+list1.grid(row=2, column=0, rowspan=6, columnspan=2)
 
-list2 = Listbox(window, height=8, width=55)
-list2.grid(row=2, column=4)
-# list2.grid(row=2, column=4, rowspan=6, columnspan=2)
+sb1 = Scrollbar(window)
+sb1.grid(row=2, column=2, rowspan=6)
 
-# sb1 = Scrollbar(window)
-# sb1.grid(row=2, column=4, rowspan=6)
+list1.configure(yscrollcommand=sb1.set)
+sb1.configure(command=list1.yview())
+list1.bind("<<ListboxSelect>>", get_selected_row)
 
-# list1.configure(yscrollcommand=sb1.set)
-# sb1.configure(command=list1.yview())
-# list1.bind("<<ListboxSelect>>", get_selected_row)
+b1 = Button(window, text="View all", width=12, command=view_command)
+b1.grid(row=2, column=3)
+
+b2 = Button(window, text="Search", width=12, comman=search_command)
+b2.grid(row=3, column=3)
+
+b4 = Button(window, text="Add", width=12, command=add_command)
+b4.grid(row=4, column=3)
+
+b5 = Button(window, text="Update selected", width=12, command=update_command)
+b5.grid(row=5, column=3)
+
+b3 = Button(window, text="Delete", width=12, command=delete_command)
+b3.grid(row=6, column=3)
+
+b6 = Button(window, text="Close", width=12, command=window.destroy)
+b6.grid(row=7, column=3)
 
 window.mainloop()
